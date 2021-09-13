39feed84fe564dfe68282403d437aebd1668888c
Taylor Barnabic <tbarnabic1@gmail.com>
2021-09-13 15:59:36 -0400

    Created images table in database

---
+++ backend/db/migrations/20210913195622-create-image.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('Images', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			spotId: {
+				allowNull: false,
+				type: Sequelize.INTEGER,
+			},
+			url: {
+				allowNull: false,
+				type: Sequelize.STRING,
+				unique: true,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('Images');
+	},
+};
---
+++ backend/db/models/image.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+	const Image = sequelize.define(
+		'Image',
+		{
+			spotId: {
+				allowNull: false,
+				type: DataTypes.INTEGER,
+			},
+			url: {
+				allowNull: false,
+				type: DataTypes.STRING,
+				unique: true,
+			},
+		},
+		{}
+	);
+	Image.associate = function (models) {
+		// associations can be defined here
+	};
+	return Image;
+};
