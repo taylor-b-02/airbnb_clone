00ba7d647a4928d3c64765883e70fc330d60176a
Taylor Barnabic <tbarnabic1@gmail.com>
2021-09-14 11:50:01 -0400

    Basic setup for backend routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,19 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const spotsRouter = require('./spots.js');
+const bookingsRouter = require('./bookings.js');
+const reviewsRouter = require('./reviews.js');
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.use('/spots', spotsRouter);
+
+router.use('/bookings', bookingsRouter);
+
+router.use('/reviews', reviewsRouter);
 
 router.post('/test', function (req, res) {
 	res.json({ requestBody: req.body });
