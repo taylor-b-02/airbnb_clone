166e1bfbfa3fcd478f769973957b13e7d0cf8bd4
Taylor Barnabic <tbarnabic1@gmail.com>
2021-09-20 07:12:39 -0400

    Updated userId for BookingsPage

---
+++ frontend/src/components/BookingsPage/BookingCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './BookingCard.css';
+const BookingCard = ({ spotName, startDay, endDay, price }) => {
+	const endObj = new Date(endDay);
+	const startObj = new Date(startDay);
+
+	const days = (endObj.getTime() - startObj.getTime()) / (1000 * 3600 * 24);
+
+	return (
+		<>
+			<div id="booking-list-card">
+				<img
+					src="https://cdn.shortpixel.ai/spai/w_2688+q_lossless+ret_img+to_webp/https://helloavenir.com/wp-content/uploads/2020/06/DSC05610.jpg"
+					alt="Spot"
+					id="booking-card-thumbnail"
+				/>
+				<span id="booking-name-span">{spotName}</span>
+				<span id="spot-price-span">{`$${price * days}`}</span> for{' '}
+				{days} nights
+				<div id="spot-price"></div>
+			</div>
+		</>
+	);
+};
+
+export default BookingCard;
--- frontend/src/components/BookingsPage/index.js
+++ frontend/src/components/BookingsPage/index.js
@@ -1,24 +1,38 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+import BookingCard from './BookingCard';
 import * as bookingActions from '../../store/booking';
+import './BookingsPage.css';
 
 const BookingsPage = () => {
 	const dispatch = useDispatch();
-	// const sessionUser = useSelector((state) => state.session.user);
+	const sessionUser = useSelector((state) => state.session.user);
 	const bookings = useSelector((state) => state.booking.bookings);
+	let userId;
+	if (sessionUser) userId = sessionUser.id;
 
 	useEffect(() => {
-		dispatch(bookingActions.getBookingsById(4));
+		dispatch(bookingActions.getBookingsById(userId));
 	}, [dispatch]);
 
 	if (!bookings) return null;
 
 	return (
 		<div id="bookings-container">
-			<h1>Your Bookings</h1>
+			<div id="bookings-heading">
+				<h1>Your Bookings</h1>
+			</div>
 			{bookings.map((booking) => {
-				return <div key={booking.id}>{booking.Spot.name}</div>;
+				return (
+					<BookingCard
+						key={booking.id}
+						spotName={booking.Spot.name}
+						price={booking.Spot.price}
+						startDay={booking.startDate}
+						endDay={booking.endDate}
+					/>
+				);
 			})}
 		</div>
 	);
