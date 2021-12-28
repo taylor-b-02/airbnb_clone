e3954085748562deea497c82fe3245b7c08cf5b1
Taylor Barnabic <tbarnabic1@gmail.com>
2021-12-27 21:17:09 -0500

    Began restructuring of home page, image added (needs more work)

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -2,67 +2,16 @@ import React from 'react';
 // import * as sessionActions from '../../store/session';
 // import { useDispatch, useSelector } from 'react-redux';
 import ExploreButton from './ExploreButton';
-import './Home.css';
+import css from './Home.module.css';
 
 const Home = () => {
 	return (
-		<div id="home-container">
-			<img
-				src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-				id="main-pic"
-				alt="Splash Page"
-			/>
-			<div id="explore-nearby-span">Explore Nearby</div>
-			<div id="explore-container">
-				<ExploreButton
-					name="New York"
-					travelTime="45 minute"
-					imageURL="https://a0.muscache.com/im/pictures/be4d3ba5-08d7-4afe-95a7-f2da6453886a.jpg?im_q=medq&im_w=240"
-					className="explore-button"
-				/>
-				<ExploreButton
-					name="Washington"
-					travelTime="5 hour"
-					imageURL="https://a0.muscache.com/im/pictures/7253e011-7c22-48fd-b75d-d0da35372397.jpg?im_q=medq&im_w=240"
-					className="explore-button"
-				/>
-				<ExploreButton
-					name="Philadelphia"
-					travelTime="2.5 hour"
-					imageURL="https://a0.muscache.com/im/pictures/76e5f1c6-a788-418c-a28b-f0ee29cffd41.jpg?im_q=medq&im_w=240"
-					className="explore-button"
-				/>
-				<ExploreButton
-					name="Boston"
-					travelTime="5 hour"
-					imageURL="https://a0.muscache.com/im/pictures/847cfb7f-788d-42dc-9148-f375348dde76.jpg?im_q=medq&im_w=240"
-					className="explore-button"
-				/>
-				<ExploreButton
-					name="Arlington"
-					travelTime="5 hour"
-					imageURL="https://a0.muscache.com/im/pictures/560c06e1-a396-4414-9e38-4fbe8e9f04c4.jpg?im_q=medq&im_w=240"
-					className="explore-button"
-				/>
-				<ExploreButton
-					name="Portland"
-					travelTime="7 hour"
-					imageURL="https://a0.muscache.com/im/pictures/71ae2609-6082-4f31-aa20-8629d7fab7d7.jpg?im_q=medq&im_w=240"
-					className="explore-button"
-				/>
-				<ExploreButton
-					name="North Bergen"
-					travelTime="30 minute"
-					imageURL="https://a0.muscache.com/im/pictures/a161fb95-6875-4aaa-aecd-3a46dead3220.jpg?im_q=medq&im_w=240"
-					className="explore-button"
-				/>
-				<ExploreButton
-					name="Baltimore"
-					travelTime="4.5 hour"
-					imageURL="https://a0.muscache.com/im/pictures/f3ebcc73-30b2-469f-b6ba-0578fdaaa649.jpg?im_q=medq&im_w=240"
-					className="explore-button"
-				/>
+		<div id={css.container}>
+			<div id={css.upperContainer}></div>
+			<div id={css.contentContainer}>
+				<img id={css.mainImage} src="https://a0.muscache.com/im/pictures/53e51dcb-8fad-4ce8-b61c-8a7a369267bf.jpg?im_q=highq&im_w=1920"/>
 			</div>
+			<footer id={css.footerContainer}></footer>
 		</div>
 	);
 };
