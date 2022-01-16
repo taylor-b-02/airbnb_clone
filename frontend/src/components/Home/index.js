898a8657d739de973a0c2274d71ec68b5c9bb7cf
Taylor Barnabic <tbarnabic1@gmail.com>
2022-01-16 17:36:47 -0500

    NavBar roughly complete, needs search field in the future

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,4 +1,20 @@
 import React from 'react';
+import {AiFillLinkedin, AiFillGithub, AiOutlinePhone, AiFillPhone, AiFillMail, AiOutlineMail} from 'react-icons/ai';
+import {
+    SiReact,
+    SiJavascript,
+    SiJava,
+    SiPython,
+    SiExpress,
+    SiFlask,
+    SiHtml5,
+    SiCss3,
+    SiPostgresql,
+    SiDocker,
+    SiSocketdotio,
+    SiAmazonaws,
+    SiNodedotjs
+} from 'react-icons/si';
 // import * as sessionActions from '../../store/session';
 // import { useDispatch, useSelector } from 'react-redux';
 // import ExploreButton from './ExploreButton';
@@ -11,7 +27,7 @@ const Home = () => {
             <div id={css.lowerBackground}>
                 <div id={css.upperBackground}>
                     <div id={css.contentContainer}>
-                        <div className={css.contentCard}>
+                        <div className={css.contentCard} id={css.imageContainer}>
                             <img id={css.mainImage}
                                  alt={"Stylish camping in the woods"}
                                  src="https://a0.muscache.com/im/pictures/53e51dcb-8fad-4ce8-b61c-8a7a369267bf.jpg?im_q=highq&im_w=1920"/>
@@ -23,8 +39,8 @@ const Home = () => {
                         <div className={css.contentCard} id={css.giftCardContainer}>
                             <div id={css.leftGCContainer}>
                                 <div id={css.littleHeaderGC}>Introducing</div>
-                                {/*<div id={css.bigHeaderGC}>Airbnb gift cards</div>*/}
-                                <div id={css.bigHeaderGC}>Taylor Barnabic</div>
+                                <div id={css.bigHeaderGC}>Airbnb gift cards</div>
+                                {/*<div id={css.bigHeaderGC}>Taylor Barnabic</div>*/}
                                 <div id={css.GCButton}>
                                     {/*<span id={css.GBButtonText}>Shop now</span>*/}
                                     <a href={"/about"}><span id={css.GBButtonText}>Learn more</span></a>
@@ -33,14 +49,31 @@ const Home = () => {
                             <div id={css.rightGCContainer}>
                                 <div id={css.businessCard}>
                                     <div id={css.businessCardText}>
-                                        <div>Taylor Barnabic</div>
-                                        <div>Full Stack Web Developer</div>
-                                        <div>Linkedin (Replace w Logo)</div>
-                                        <div>Github (Replace w Logo) taylor-b-02</div>
-                                        <div>Phone #: <span id={css.hiddenContent}>(201)-983-9337</span></div>
-                                        <div>Email: <span id={css.hiddenContent}>tbarnabic@gmail.com</span></div>
+                                        <div id={css.bcName}>Taylor Barnabic</div>
+                                        <div id={css.bcTitle}>Full Stack Web Developer</div>
+                                        <div><a href={"https://www.linkedin.com/in/taylor-barnabic-63892a20a"}
+                                                target={"_blank"} id={css.bcLink}><AiFillLinkedin/> Linkedin</a></div>
+                                        <div><a href={"https://github.com/taylor-b-02"}
+                                                target={"_blank"} id={css.bcLink}><AiFillGithub/> taylor-b-02</a></div>
+                                        <div><AiFillPhone/> <span id={css.hiddenContent}>(201)-983-9337</span></div>
+                                        <div><a href={"mailto:tbarnabic@gmail.com"} id={css.bcLink}>
+                                            <AiFillMail/> tbarnabic@gmail.com
+                                        </a>
+                                        </div>
                                     </div>
-                                    <div id={css.businessCardPic}></div>
+                                    <div id={css.businessCardRight}>
+                                        <SiHtml5 title={"HTML5"}/>
+                                        <SiCss3 title={"CSS3"}/>
+                                        <SiJavascript title={"JavaScript"}/>
+                                        <SiReact title={"React"}/>
+                                        <SiExpress title={"Express"}/>
+                                        <SiNodedotjs title={"node.js"}/>
+                                        <SiPython title={"python"}/>
+                                        <SiFlask title={"Flask"}/>
+                                        <SiPostgresql title={"PostgreSQL"}/>
+                                        <SiSocketdotio title={"Socket.IO"}/>
+                                        <SiDocker title={"docker"}/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -53,7 +86,7 @@ const Home = () => {
                                 <LocationCard bgColor={"#D93A30"} name={"Los Angeles"} distance={68}/>
                             </div>
                         </div>
-                        <footer id={css.footerContainer}>Foot</footer>
+                        {/*<footer id={css.footerContainer}>Foot</footer>*/}
                     </div>
                 </div>
             </div>
